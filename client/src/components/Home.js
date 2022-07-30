a09256e56441780e7626995cd4f8a94103db133d
Vikram Singh <vs125609@gmail.com>
2022-07-30 11:21:05 +0530

    material ui added

---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './Navbar';
+const Home = () => {
+    return(
+        <>
+            <Navbar/>
+            <h1>Welcome To My Portfolio Website</h1>
+            <p>I am MERN STACK DEVELOPER</p>
+        </>
+    );
+};
+export default Home;
